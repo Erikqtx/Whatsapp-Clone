88ffa47215d3ae91ace74ae60af1373cd3be838e
Erikqtx <glaser-erik@web.de>
2020-09-07 18:55:32 +0200

    Add Firebase Config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9fgFR5beihGtMSvKNEcRGq5X4q1QDRzw",
+  authDomain: "whatsapp-clone-8228d.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-8228d.firebaseio.com",
+  projectId: "whatsapp-clone-8228d",
+  storageBucket: "whatsapp-clone-8228d.appspot.com",
+  messagingSenderId: "60259138940",
+  appId: "1:60259138940:web:5a39066b77d0a4461f78f9",
+};
