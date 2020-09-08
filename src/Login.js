8c3e77c66ac45325fdf31aa627ba8dfaeb244a02
Erikqtx <glaser-erik@web.de>
2020-09-08 16:45:02 +0200

    Finishing Google Auth and Adding Avatar Pictures

--- src/Login.js
+++ src/Login.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./Login.css";
 import { Button } from "@material-ui/core";
 import { auth, provider } from "./firebase";
-import { AlternateEmail } from "@material-ui/icons";
+import { actionTypes } from "./reducer";
+import { useStateValue } from "./StateProvider";
 function Login() {
+  const [{}, dispatch] = useStateValue();
   const signIn = () => {
     auth
       .signInWithPopup(provider)
-      .then((result) => console.log(result))
+      .then((result) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
       .catch((error) => alert(error.message));
   };
   return (
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,8 +7,10 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined, Unsubscribe } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat.js";
 import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 function Sidebar() {
   const [rooms, setRooms] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
   useEffect(() => {
     const unsubscribe = db.collection("rooms");
     db.collection("rooms").onSnapshot((snapshot) =>
@@ -26,7 +28,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar />
+        <Avatar src={user?.photoURL} />
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
